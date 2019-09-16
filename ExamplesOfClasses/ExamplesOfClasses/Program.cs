7ebb0d33dfbef9f410e69e9c80017dd680c0e11b
Igor Korovin <54861554+ikorovin@users.noreply.github.com>
2019-09-16 14:34:03 -0500

    Completed ExamplesOfClasses

---
+++ ExamplesOfClasses/ExamplesOfClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamplesOfClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Name = "nintendo.switch";
+            product.Price = 299.99M;
+            Console.WriteLine($"{product.Name} ${product.Price}");
+            Console.ReadLine();
+        }
+    }
+}
